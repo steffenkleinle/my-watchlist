cf5e7e90359f7b3cd7d0b388915e6637571b62fc
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-12-08 14:50:45 +0100

    Fix merge

--- app/src/main/java/app/mywatchlist/MainActivity.kt
+++ app/src/main/java/app/mywatchlist/MainActivity.kt
@@ -10,12 +10,30 @@ import androidx.compose.ui.Modifier
 import app.mywatchlist.ui.theme.MyWatchlistTheme
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import app.mywatchlist.ui.home.HomeViewModel
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.uiState.map { it.watchables }.distinctUntilChanged()
+                    .collect { uiState ->
+                        println(uiState)
+                    }
+            }
+        }
+
         setContent {
 
             Button(onClick = { /* Do something! */ }) { Text("Button") }
