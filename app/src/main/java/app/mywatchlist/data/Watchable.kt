49a1abbf71def78b54f609be18e5279f87928627
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-12-12 19:35:39 +0100

    Add providers by region endpoint

--- app/src/main/java/app/mywatchlist/data/Watchable.kt
+++ app/src/main/java/app/mywatchlist/data/Watchable.kt
@@ -20,7 +20,3 @@ data class Watchable(
     @Json(name = "vote_count") val voteCount: Int,
     val providers: Map<String, Providers>? = null
 )
-
-data class Watchables(
-    @Json(name = "results") val watchables: List<Watchable>
-)
