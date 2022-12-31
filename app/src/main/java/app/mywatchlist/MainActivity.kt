35e845f8e7296de4bd4d7888408cf5e9514977d6
Kränzle <stefan.kraenzle@opitz-consulting.com>
2022-12-31 11:01:22 +0100

    Built Offline Notice for UI

--- app/src/main/java/app/mywatchlist/MainActivity.kt
+++ app/src/main/java/app/mywatchlist/MainActivity.kt
@@ -8,7 +8,22 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import app.mywatchlist.screens.MainScreen
+import app.mywatchlist.utils.ConnectivityObserver
+import app.mywatchlist.utils.NetworkConnectivityObserver
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,10 +50,36 @@ object DataStoreModule {
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //TODO Maybe a bad idea to put this here, rather put it in a ViewModel
+        var connectivityObserver = NetworkConnectivityObserver(applicationContext)
+
         super.onCreate(savedInstanceState)
 
         setContent {
+
+            val connectionStatus by connectivityObserver.observe().collectAsState(
+                initial = ConnectivityObserver.Status.Unavailable
+            )
+            if (connectionStatus === ConnectivityObserver.Status.Lost){
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .zIndex(10f)
+                    .background(MaterialTheme.colors.error)
+                    .padding(0.dp, 3.dp),
+                    contentAlignment = Alignment.TopCenter,){
+                    Row() {
+                        Icon(Icons.Default.Warning, "Lost connection")
+                        androidx.compose.material.Text(
+                            text = "You're offline",
+                            fontSize = MaterialTheme.typography.h6.fontSize,
+                        )
+                    }
+                }
+            }
+
             MainScreen()
         }
     }
--- app/src/main/java/app/mywatchlist/screens/MainScreen.kt
+++ app/src/main/java/app/mywatchlist/screens/MainScreen.kt
@@ -1,13 +1,21 @@
 package app.mywatchlist.screens
 
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.snapshots.Snapshot.Companion.observe
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -17,11 +25,15 @@ import androidx.navigation.compose.rememberNavController
 import app.mywatchlist.BottomBarScreen
 import app.mywatchlist.Screen
 import app.mywatchlist.navigation.SetupNavGraph
+import app.mywatchlist.utils.ConnectivityObserver
+import app.mywatchlist.utils.NetworkConnectivityObserver
+
 
 @Composable
 fun MainScreen() {
 
     val navController = rememberNavController()
+
     Scaffold(
         bottomBar = { BottomBar(navController) }
     ) {
