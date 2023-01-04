c37a6c0e9dbf7d9a1a3744e3572e97a7320526a4
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-01-04 14:40:36 +0100

    Fixes (#35)
    * Fix date parsing
    
    * Fix search bar and query state
    
    * Fix endless loading if unavailable

--- app/src/main/java/app/mywatchlist/ui/viewModels/WatchtablesViewModel.kt
+++ app/src/main/java/app/mywatchlist/ui/viewModels/WatchtablesViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.mywatchlist.data.repositories.WatchablesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,19 +14,24 @@ class WatchablesViewModel @Inject constructor(
     private val repository: WatchablesRepository
 ) : ViewModel() {
     var uiState = repository.trendingFlow().asStateFlow(viewModelScope)
-    private var query: String? = null
+    private val _queryUiState = MutableStateFlow<String?>(null)
+    val queryUiState = _queryUiState.asStateFlow()
 
     fun update(query: String?) {
         val normalized = query.normalize()
-        if (this.query != normalized) {
+        val previous = this.queryUiState.value
+        val previousNormalized = previous.normalize()
+
+        if (query != previous) {
+            _queryUiState.update { query }
+        }
+
+        if (normalized != previousNormalized) {
             uiState = (
                     if (normalized == null) repository.trendingFlow()
                     else repository.searchFlow(normalized)
                     ).asStateFlow(viewModelScope)
-
         }
-
-        this.query = normalized
     }
 
     private fun String?.normalize() = this?.trim()?.ifEmpty { null }
