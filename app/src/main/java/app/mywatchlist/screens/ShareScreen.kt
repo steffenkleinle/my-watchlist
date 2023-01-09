5af01cbbdcf8cc6eb3e03eac226518990792db13
Kränzle <stefan.kraenzle@opitz-consulting.com>
2023-01-09 15:08:52 +0100

    Able to select a contact

--- app/src/main/java/app/mywatchlist/screens/ShareScreen.kt
+++ app/src/main/java/app/mywatchlist/screens/ShareScreen.kt
@@ -1,24 +1,41 @@
 package app.mywatchlist.screens
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import app.mywatchlist.R
-import kotlinx.coroutines.launch
+import app.mywatchlist.utils.hasContactPermission
+import app.mywatchlist.utils.requestContactPermission
 
 @Composable
-fun ShareScreen(
+fun ShareScreen (
     navController: NavController,
     watchableTitle: String?
 ) {
@@ -67,22 +84,117 @@ fun ShareScreen(
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center
             ){
-                Button(
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colors.primary,
-                        contentColor = MaterialTheme.colors.background
-                    ),
-                    onClick = {
-                       /*dkdk*/
-                        })
-                     {
-                    Icon(Icons.Default.Person, stringResource(R.string.select_Contact))
-                    Text(
-                        text = stringResource(R.string.select_Contact),
-                    )
-                }
+                contactPicker()
             }
         }
+    }
+}
 
+
+@SuppressLint("Range")
+@Composable
+fun contactPicker(
+) {
+
+    var contactName by remember { mutableStateOf("") }
+    var contactNumber by remember {  mutableStateOf("")}
+    val activity = LocalContext.current as Activity
+
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickContact(),
+        onResult = {
+            val contentResolver: ContentResolver = context.contentResolver
+            var name = ""
+            var number = ""
+            val cursor: Cursor? = contentResolver.query(it!!, null, null, null, null)
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    Log.d("Name", name)
+                    val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                    val phones: Cursor? = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null
+                    )
+                    if (phones != null) {
+                        while (phones.moveToNext()) {
+                            number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            Log.d("Number", number)
+                        }
+                        phones.close()
+                    }
+                }
+            }
+            contactName = name
+            contactNumber = number
+        }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+        Text(
+            text = stringResource(R.string.share_with),
+            color = MaterialTheme.colors.onBackground,
+            fontSize = MaterialTheme.typography.h5.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+        Button(
+            modifier = Modifier.padding(0.dp, 20.dp),
+            onClick = {
+                if (hasContactPermission(context)) {
+                    launcher.launch()
+                } else {
+                    requestContactPermission(context, activity)
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.background
+            )
+        )
+        {
+            Icon(Icons.Default.Person, stringResource(R.string.select_Contact))
+            Text(
+                text = stringResource(R.string.select_Contact),
+            )
+        }
+
+        Text(
+            text = contactName,
+            color = MaterialTheme.colors.onBackground,
+            fontSize = MaterialTheme.typography.h6.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = contactNumber,
+            color = MaterialTheme.colors.onBackground,
+        )
+
+        if (!contactName.isNullOrEmpty()){
+            Button(
+                modifier = Modifier.padding(0.dp, 20.dp),
+                onClick = {/* Share */},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colors.primary,
+                    contentColor = MaterialTheme.colors.background
+                )
+            )
+            {
+                Icon(Icons.Default.Send, stringResource(R.string.share))
+                Text(
+                    text = stringResource(R.string.share),
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/java/app/mywatchlist/utils/PermissionRequestFunctions.kt
@@ -0,0 +1,19 @@
+package app.mywatchlist.utils
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+fun hasContactPermission(context: Context): Boolean {
+    return ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) ==
+            PackageManager.PERMISSION_GRANTED;
+}
+
+fun requestContactPermission(context: Context, activity: Activity) {
+    if (!hasContactPermission(context)) {
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_CONTACTS), 1)
+    }
+}
